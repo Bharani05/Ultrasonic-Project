ec22b572f160bf08f375ad3a813b8aed71304526
Bharani05 <113593637+Bharani05@users.noreply.github.com>
2024-07-25 09:44:45 +0530

    Update procssfile.c

--- procssfile.c
+++ procssfile.c
@@ -228,4 +228,120 @@ module_exit(my_exit);
 /home/lg-bharani/drivers/bharani/procfile.c:29:5: error: implicit declaration of function ‘uname’; did you mean ‘utsname’? [-Werror=implicit-function-declaration]
    29 |     uname(&sysinfo); // Get system information
       |     ^~~~~
+// update
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+
+#define BUFSIZE  100
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Bharani<bhrnbala@gmail.com>");
+MODULE_DESCRIPTION("Module creates a folder and file in procfs and implements read and write callbacks");
+
+static struct proc_dir_entry *proc_folder;
+static struct proc_dir_entry *proc_file;
+
+static ssize_t my_read(struct file *file, char *user_buffer, size_t count, loff_t *offs)
+{
+    static const char *proc_cpuinfo_path = "/proc/cpuinfo";
+    struct file *cpuinfo_file;
+    char *cpuinfo_buf;
+    loff_t pos = 0;
+    ssize_t ret;
+    mm_segment_t oldfs;
+
+    if (*offs > 0) {
+        return 0; // End of file
+    }
+
+    cpuinfo_file = filp_open(proc_cpuinfo_path, O_RDONLY, 0);
+    if (IS_ERR(cpuinfo_file)) {
+        printk(KERN_ERR "procfs_test - Error opening %s\n", proc_cpuinfo_path);
+        return PTR_ERR(cpuinfo_file);
+    }
+
+    cpuinfo_buf = kmalloc(BUFSIZE, GFP_KERNEL);
+    if (!cpuinfo_buf) {
+        filp_close(cpuinfo_file, NULL);
+        return -ENOMEM;
+    }
+
+    oldfs = get_fs();
+    set_fs(KERNEL_DS);
+    ret = kernel_read(cpuinfo_file, cpuinfo_buf, BUFSIZE - 1, &pos);
+    set_fs(oldfs);
+    filp_close(cpuinfo_file, NULL);
+
+    if (ret < 0) {
+        kfree(cpuinfo_buf);
+        return ret;
+    }
+
+    cpuinfo_buf[ret] = '\0'; // Null-terminate the buffer
+
+    if (copy_to_user(user_buffer, cpuinfo_buf, ret)) {
+        kfree(cpuinfo_buf);
+        return -EFAULT;
+    }
+
+    kfree(cpuinfo_buf);
+    *offs += ret;
+    return ret;
+}
+
+static ssize_t my_write(struct file *file, const char *user_buffer, size_t count, loff_t *offs)
+{
+    char text[255];
+    int to_copy, not_copied, delta;
+
+    memset(text, 0, sizeof(text));
+
+    to_copy = min(count, sizeof(text) - 1);
+
+    not_copied = copy_from_user(text, user_buffer, to_copy);
+    printk(KERN_INFO "procfs_test - You have written %s to our proc entry\n", text);
+
+    delta = to_copy - not_copied;
+    return delta;
+}
+
+static struct proc_ops fops =
+{
+    .proc_read = my_read,
+    .proc_write = my_write,
+};
+
+static int __init my_init(void)
+{
+    proc_folder = proc_mkdir("bharani", NULL);
+    if (proc_folder == NULL) {
+        printk(KERN_ERR "procfs_test - Error creating /proc/bharani\n");
+        return -ENOMEM;
+    }
+
+    proc_file = proc_create("dummy", 0666, proc_folder, &fops);
+    if (proc_file == NULL) {
+        printk(KERN_ERR "procfs_test - Error creating /proc/bharani/dummy\n");
+        proc_remove(proc_folder);
+        return -ENOMEM;
+    }
+    printk(KERN_INFO "procfs_test - Created /proc/bharani/dummy\n");
+    return 0;
+}
+
+static void __exit my_exit(void)
+{
+    printk(KERN_INFO "procfs_test - Removing /proc/bharani/dummy\n");
+    proc_remove(proc_file);
+    proc_remove(proc_folder);
+}
+
+module_init(my_init);
+module_exit(my_exit);
 
