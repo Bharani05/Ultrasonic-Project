bd8ed1b57643bb2f212e1c08d22e359d3aa85a35
Bharani05 <113593637+Bharani05@users.noreply.github.com>
2024-08-02 16:28:43 +0530

    Update thread.c

--- thread.c
+++ thread.c
@@ -31,3 +31,82 @@ int main()
         printf("main thread is exiting now:\n");
         pthread_exit(NULL);
 }
+//thread_cancel
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+
+void *thread_function(void *arg)
+{
+        char *s = (char *) arg;
+        int count =0 ;
+        printf("Thread: thread_function is started\n");
+        while(1){
+                sleep(1);
+                printf("\nthread_function: executing loop\n");
+        }
+        sleep(5);
+
+        printf("\nThread: thread_fuction exiting\n");
+}
+int main()
+{
+        pthread_t t_id;
+//      void *res;
+        int s, count;
+
+        printf("\nMain thread is started:\n");
+        s = pthread_create(&t_id, NULL, thread_function, NULL);
+        if(s!=0)
+                printf("\nProblem in thread creation \n");
+        else
+                printf("\nThread created Successfully:\n");
+        for(count = 0; count < 5; count ++){
+                sleep(2);
+                printf("\nMain thread: count value = %d \n", count);
+        }
+        pthread_cancel(t_id);
+
+        printf("\nMain thread exiting \n");
+        pthread_exit(NULL);
+}
+//thread.c
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+void *thread_function(void *args)
+{
+        printf("Hello i am testing my thread concept\n");
+        sleep(30);
+}
+
+int main()
+{
+        pthread_t t_id;
+        int ret;
+
+        printf("Process id = %d \n", getpid());
+        printf("Before creation of thread\n");
+
+        ret = pthread_create(&t_id, NULL, thread_function, NULL);
+        if (ret == 0)
+                printf("thread created successfully\n");
+        else
+                printf("Problem in thread creation\n");
+
+        pthread_join(t_id, NULL);
+        if(ret !=0)
+                printf("pthread_join error\n");
+        else
+                printf("Thread terminated\n");
+        return 0;
+}
